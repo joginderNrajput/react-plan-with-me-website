2c204075a198c3c0665c5d4820cd054d14849263
joginder <jogindersinghudh2003Wgmail.com>
2023-04-10 09:42:36 +0530

    intial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, {useState} from "react";
+import data from "./data.js";
+import Tours from "./components/Tours";
+
+const App = () => {
+  
+  function removeTour(id){
+    const newTours = tours.filter(tour => tour.id !== id);
+    setTours(newTours);
+  }
+
+  const [tours, setTours] = useState(data);
+  if(tours.length === 0){
+    return(
+      <div className="refresh">
+        <h2>No Tour Left</h2>
+        <button className="btn-white" onClick={() => setTours(data)}> 
+          Refresh
+        </button>
+      </div>
+    )
+  }
+
+ return (
+    <div className="App">
+      <Tours tours = {tours} removeTour = {removeTour}></Tours>
+    </div>
+  )
+};
+
+export default App;
